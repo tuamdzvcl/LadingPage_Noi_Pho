8930830e0a6ff78bb834557eeb7af50eac56defa
phamngoctuananh <phamngoctuananh161@gmail.com>
2026-08-22 09:21:32 +0700

    fix tên

--- testimonials.js
+++ testimonials.js
@@ -2,43 +2,53 @@ const testimonialsData = [
   {
     id: 1,
     stars: 5,
-    content: "Trước cứ nghĩ là nồi nấu phở bằng mâm nhiệt k sôi liu riu được. Mà mua về có đủ chế độ từ sôi mạnh đến sôi liu riu rất tiện.",
+    content:
+      "Trước cứ nghĩ là nồi nấu phở bằng mâm nhiệt k sôi liu riu được. Mà mua về có đủ chế độ từ sôi mạnh đến sôi liu riu rất tiện.",
     avatar: "image/feaback1.jpg",
     name: "Nguyễn Luyến",
     date: "2024-04-15 21:16",
     images: ["image/noi3.jpg", "image/noi4.jpg"],
-    tags: ["Chất lượng sản phẩm tuyệt vời", "Đóng gói sản phẩm rất đẹp và chắc chắn", "Rất đáng tiền"],
-    reply: "Cảm ơn bạn đã tin tưởng và lựa chọn sản phẩm của chúng tôi."
+    tags: [
+      "Chất lượng sản phẩm tuyệt vời",
+      "Đóng gói sản phẩm rất đẹp và chắc chắn",
+      "Rất đáng tiền",
+    ],
+    reply: "Cảm ơn bạn đã tin tưởng và lựa chọn sản phẩm của chúng tôi.",
   },
   {
     id: 2,
     stars: 5,
-    content: "Dùng nồi thanh nhiệt cứ hay bị cháy. Dùng thử sang nồi hãng này vì thấy có cái chống cháy! Tôi đã tét thử thấy rất ưng. Đúng như quảng cáo.",
+    content:
+      "Dùng nồi thanh nhiệt cứ hay bị cháy. Dùng thử sang nồi hãng này vì thấy có cái chống cháy! Tôi đã tét thử thấy rất ưng. Đúng như quảng cáo.",
     avatar: "image/feaback6.jpg",
     name: "Nguyễn Thị Hồng",
     date: "2024-03-20 09:45",
     images: ["image/noi5.jpg"],
     tags: ["Chất lượng sản phẩm tuyệt vời", "Giao hàng nhanh"],
-    reply: "Điện máy Atlantis rất vui khi sản phẩm giúp ích được cho quán của cô. Chúc quán luôn đông khách ạ!"
+    reply:
+      "Điện máy Atlantis rất vui khi sản phẩm giúp ích được cho quán của cô. Chúc quán luôn đông khách ạ!",
   },
   {
     id: 3,
     stars: 5,
     content: "Nhận nồi về có hơi móp 1 tí nhưng bên này hỗ trợ rất nhiệt tình!",
     avatar: "image/feaback3.jpg",
-    name: "Nguyễn Văn Sáng",
+    name: "Ngô Văn Tuấn Kiệt ",
     date: "2024-02-12 14:30",
     tags: ["Dịch vụ tận tâm", "Sản phẩm chất lượng"],
-    reply: "Cảm ơn chú Sáng đã tin dùng sản phẩm của Atlantis. Đội ngũ kỹ thuật luôn sẵn sàng hỗ trợ 24/7 ạ."
+    reply:
+      "Cảm ơn chú Sáng đã tin dùng sản phẩm của Atlantis. Đội ngũ kỹ thuật luôn sẵn sàng hỗ trợ 24/7 ạ.",
   },
   {
     id: 4,
     stars: 5,
-    content: "Điều làm tôi thích nhất ở Atlantis là lớp vỏ chống nóng 3 lớp. Nhà đông cháu nhỏ chạy qua lại mà chạm vào vỏ nồi chỉ thấy hơi ấm ấm chứ không lo bỏng.",
+    content:
+      "Điều làm tôi thích nhất ở Atlantis là lớp vỏ chống nóng 3 lớp. Nhà đông cháu nhỏ chạy qua lại mà chạm vào vỏ nồi chỉ thấy hơi ấm ấm chứ không lo bỏng.",
     avatar: "image/feaback4.jpg",
     name: "Hoàng Quốc Bảo",
     date: "2024-01-05 10:15",
     tags: ["An toàn khi sử dụng", "Thiết kế thông minh"],
-    reply: "Cảm ơn chú đã phản hồi tích cực. Sự an toàn của khách hàng luôn là ưu tiên hàng đầu của Atlantis."
-  }
+    reply:
+      "Cảm ơn chú đã phản hồi tích cực. Sự an toàn của khách hàng luôn là ưu tiên hàng đầu của Atlantis.",
+  },
 ];
